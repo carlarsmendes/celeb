25b767e8953eb641be90852c7f8f4bbb6f168a5e
Carla Mendes <carlarsmendes@gmail.com>
2019-05-14 17:10:20 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -1,16 +1,77 @@
 // import React from 'react';
 import axios from 'axios';
 import React, { Component } from 'react';
-import { Route, NavLink } from "react-router-dom";
+
+// import { Route, NavLink } from "react-router-dom";
 
 import './App.css';
-import CelebDetails from './CelebDetails';
+// import CelebDetails from './CelebDetails';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = { 
-      celebrities: [] }
+      celebrities: [],
+      selectedCelebrity: null,
+      search: "" }
+      this.changeSearch = this.changeSearch.bind(this)
+  }
+  selectCelebrity(celebrity){
+      this.setState({
+        selectedCelebrity: celebrity
+      })
+  }
+
+  changeSearch(e){
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  render() {
+    return(
+    <div className="App">
+
+    <h1 className="header">Movie Celebrities</h1>
+    <p>{this.state.celebrities.length} Celebrities</p>
+    <p><input type="text" value={this.state.search} onChange={this.changeSearch} /></p>
+
+      <div className="dashboard">
+          <div className="celebs-list">
+           {!this.state.celebrities && <div>Loading...</div>}
+          {this.state.celebrities && 
+            <div>
+      {this.state.celebrities
+      .filter(celebrity => celebrity.name.toUpperCase().includes(this.state.search.toUpperCase()))
+      .map(celeb => 
+          <li 
+            key={celeb.id} 
+            className="celebs-list-item" 
+            onClick={()=>this.selectCelebrity(celeb)}>
+              {/* <NavLink to={'/'+celeb.id}>  */}
+                <img src={`https://image.tmdb.org/t/p/w185/${celeb.profile_path}`} alt="" />
+                {celeb.name}
+              {/* </NavLink>             */}
+            </li>)}
+          </div>
+          }
+        </div>
+        <div className="celeb-detail">
+          {this.state.selectedCelebrity && <div>
+            <h2>{this.state.selectedCelebrity.name}</h2>
+            <img src={`https://image.tmdb.org/t/p/w185/${this.state.selectedCelebrity.profile_path}`} alt="" />
+            <h3>Known For</h3>
+
+            {this.state.selectedCelebrity.known_for.map(movie =>
+              <div key ={movie.id}><p>{movie.title}</p>
+            <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt="" />
+            </div>)}
+        {/* <Route path="/:celebId" render={props => <CelebDetails {...props} celebrities={this.state.celebrities} />} /> */}
+          </div>}
+      </div></div> 
+    </div>
+
+  );
   }
 
   componentDidMount() {
@@ -23,60 +84,19 @@ class App extends Component {
     //       celebrities: ([resp1.data,resp2.data])
     //     })
     //   })
-
-    axios.get("https://api.themoviedb.org/3/person/popular?api_key=e22350b1c77a45e2a55c374787f0acfc&language=en-US&page=1")
     
+    //To get the data from the API
+    for(let page =1; page <= 10; page++)
+    { setTimeout(() => {
+    axios.get(`https://api.themoviedb.org/3/person/popular?api_key=e22350b1c77a45e2a55c374787f0acfc&language=en-US&page=${page}`)
       .then((response) => {
-         console.log("response------>",response.data.results);
         this.setState({
-          celebrities: response.data.results
+          celebrities: [...this.state.celebrities,...response.data.results]
         })
       })
-  }
-
-  // showCelebDetails(){
-  //   let celebDetails = this.state.celebrities.results
-  //     .find(u => u.id === this.props.match.params.celebId)
-
-  // }
-
-  render() {
-
-    
-    return(
-    <div className="App">
-
-    <h1>Movie Celebrities</h1>
-
-      <div className="dashboard">
-          <div className="celebs-list">
-           {!this.state.celebrities && <div>Loading...</div>}
-          {this.state.celebrities && 
-            <div>
-      {this.state.celebrities.map(celeb => 
-          <li key={celeb.id} className="celebs-list-item">
-              <NavLink to={'/'+celeb.id}> 
-                <img src={`https://image.tmdb.org/t/p/w185/${celeb.profile_path}`} alt="" />
-                {celeb.name}
-              </NavLink>            
-            </li>)}
-          </div>
-          }
-        </div>
-        <div className="celeb-detail">
-        {/* <Route path="/:celebId" component={CelebDetails} /> */}
-        <Route path="/:celebId" render={props => <CelebDetails {...props} celebrities={this.state.celebrities} />} />
-          {/* <h1>Celebrity Detail</h1>
-          {console.log("this.state.celebrities.results--->",this.state.celebrities)}
-        {this.state.celebrities.filter(u => u.id === this.props.match.params.celeb.id).name} */}
-      </div></div>
-      
-
-    </div>
-
-  );
-  }
-}
+    },page*100)
+    } 
+}}
 
 export default App;
 
